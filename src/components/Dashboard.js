2ebea2c20b91cddafd941954add52bb3ba407804
davidmihai3002 <159143952+davidmihai3002@users.noreply.github.com>
2025-01-12 01:48:37 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./Welcome/Welcome";
 import Homepage from "./Homepage/Homepage";
 
 const Dashboard = () =>{
     return (
-        <Router basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={<Welcome />} />
             
             <Route path="/home" element={<Homepage />} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       );
 }
 
